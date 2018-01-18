712200732ba0095471330ba084dc2f0bca522aa0
ckim618 <ckim618@gmail.com>
2018-01-17 22:06:22 -0800

    made live and code links just need to link to each correct project

--- personal/assets/js/main.js
+++ personal/assets/js/main.js
@@ -4,4 +4,15 @@ $(document).ready(function() {
 
     $('.phone').text(phone);
     $('.email').text(email).attr('href', 'mailto:'+ email);
+    $('.live').on('click', liveButton);
+    $('.code').on('click', codeButton);    
 });
+
+function liveButton() {
+    console.log('clicked')
+    window.open('https://www.thecubetube.com');
+}
+function codeButton() {
+    console.log('clicked')
+    window.open('https://github.com/ckim618/The_Cube_Tube');
+}
