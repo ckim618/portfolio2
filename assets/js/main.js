80f86660923d3b7c498a36a7288973f6f7502ac2
ckim618 <ckim618@gmail.com>
2018-01-24 12:34:15 -0800

    updated resume link

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 });
 
 function resume() {
-    window.open('https://drive.google.com/file/d/1mxhRuuxJW2_j_RDPvh26T0bmlUmDSqeW/view?usp=sharing');
+    window.open('https://drive.google.com/file/d/1CDJ9jwrurwqWuulz4f_Zqby4xFf2Pnec/view?usp=sharing');
 }
 
 function cubeTubeLiveButton() {
